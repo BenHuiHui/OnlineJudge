23333c71625f2f884778645c3d038355e316518e
BenHuiHui <qinghe1992@gmail.com>
2016-02-13 15:53:27 +0800

    ListNode

---
+++ leetcode/leetcode_util/ListNode.cpp
@@ -0,0 +1,16 @@
+//
+//  ListNode.cpp
+//  CppPlayground
+//
+//  Created by Hui Hui on 13/2/16.
+//  Copyright © 2016 Hui Hui. All rights reserved.
+//
+
+#include <stdio.h>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
---
+++ leetcode/leetcode_util/ListNode.java
@@ -0,0 +1,16 @@
+package leetcode.leetcode_util;
+
+/**
+ * Created by huihui on 3/2/16.
+ *
+ * A helper class for node used in Leetcode
+ *
+ */
+
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+
+}
