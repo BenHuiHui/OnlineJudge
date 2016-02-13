c81ae6e92a9f3fb7a744fe29c6c08347551bad53
BenHuiHui <qinghe1992@gmail.com>
2016-02-13 15:54:00 +0800

    P028

---
+++ leetcode/P028_ImplementStrStr.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * Created by huihui on 4/2/16.
+ */
+public class P028_ImplementStrStr {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+}
