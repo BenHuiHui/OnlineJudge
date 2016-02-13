af341ad6b3cc09a8053c0d75d085a788023db2ce
BenHuiHui <qinghe1992@gmail.com>
2016-02-13 15:53:40 +0800

    TreeNode

---
+++ leetcode/leetcode_util/TreeNode.cpp
@@ -0,0 +1,14 @@
+//
+//  TreeNode.cpp
+//  CppPlayground
+//
+//  Created by Hui Hui on 10/2/16.
+//  Copyright © 2016 Hui Hui. All rights reserved.
+//
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
---
+++ leetcode/leetcode_util/TreeNode.java
@@ -0,0 +1,13 @@
+package leetcode.leetcode_util;
+
+/**
+ * Created by huihui on 7/2/16.
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int x) { val = x; }
+
+}
