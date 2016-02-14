85a43606b6e3f90d1d5b324c4532e3ab3e5ea3d5
BenHuiHui <qinghe1992@gmail.com>
2016-02-14 13:12:27 +0800

    P046

---
+++ leetcode/P046_Permutations.cpp
@@ -0,0 +1,9 @@
+//
+//  P046_Permutations.cpp
+//  CppPlayground
+//
+//  Created by Hui Hui on 13/2/16.
+//  Copyright © 2016 Hui Hui. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ leetcode/P047_PermutationsII.cpp
@@ -0,0 +1,9 @@
+//
+//  P047_PermutationsII.cpp
+//  CppPlayground
+//
+//  Created by Hui Hui on 14/2/16.
+//  Copyright © 2016 Hui Hui. All rights reserved.
+//
+
+#include <stdio.h>
