f6cc82261c48965e2b90baab04b10f1f2fb4a085
BenHuiHui <qinghe1992@gmail.com>
2016-02-28 13:27:47 +0800

    P292

---
+++ leetcode/P292_NimGame.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Created by huihui on 28/2/16.
+ */
+public class P292_NimGame {
+
+    public boolean canWinNim(int n) {
+        return n%4 != 0;
+    }
+
+}
